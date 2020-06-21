43a4408e4fd427cb75e9c60b98f210730bcc728b
Zait06 <zait-hdez@hotmail.com>
2020-06-21 09:38:55 -0500

    Coordenada

--- src/js/Coordenada.js
+++ src/js/Coordenada.js
@@ -1,7 +1,8 @@
 export class Coordenada{
     constructor(x,y){
-        this.x = x;     // Coordenada X
-        this.y = y;     // Coordeanda Y
+        this.x = x;     	// Coordenada X
+        this.y = y;     	// Coordeanda Y
+		this.distance = 0	// Distancia de un punto a una coordenada
     }
 
     getX(){         // Obtener coordenada X
