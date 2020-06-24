19b4179ff0d102bdb09b1ca837a4d14e5c02d3a5
Zait06 <zait-hdez@hotmail.com>
2020-06-23 22:42:53 -0500

    PSO

--- src/app.js
+++ src/app.js
@@ -7,8 +7,16 @@ const { UI } = require('./js/UI');
 
 const ui = new UI();
 var rec = new Rectangulo(5,25,20,10);
-var part = new Particle(4);
+var part00 = new Particle('r');
+var part01 = new Particle('c');
+var population = [];
 
 ui.render(rec.getArea());
-console.log(part.getPosition());
-console.log(part.getVelocity());
+console.log(part00.getPosition());
+console.log(part01.getPosition());
+
+for(let i=0;i<10;i++){
+    population.push(new Particle('r'));   
+}
+
+console.log(population)
--- src/js/PSO.js
+++ src/js/PSO.js
@@ -1,15 +1,52 @@
 export class Particle{
-    constructor(length){
-        // position = (x1,y1,x2,y2)
-        this.position = new Array(length);   // Posicion de la partícula
-        this.velocity = new Array(length);
-        for(let i=0;i<length;i++){
-            this.position[i] = Math.random()*101;   // Posicion aleatorio
-            this.velocity[i] = Math.random()*101;   // Velocidad aleatorio
+    constructor(figura){
+        this.figure = figura;       // Figura a hacer
+        this.length = 0
+        /*
+            position
+            r = (x1,y1,x2,y2)
+            c = (x,y,r)
+        */
+        this.position = [];     // Posicion de la particula
+        this.velocity = [];     // Velocidad de particula
+        switch(figura){
+            case 'r':               // Rectángulo
+                this.length = 4;
+                this.initRect();
+            break;
+            case 'c':               // Ciruculo
+                this.length = 3;
+                this.initCirc();
+            break;
         }
         this.score = 0;
         this.best_score = 100e10;
         this.bp = []
+    }
+
+    initRect(){
+        for(let i=0;i<this.length;i++){
+            this.position[i] = Math.random()*101;   // Posicion aleatorio
+            this.velocity[i] = Math.random()*101;   // Velocidad aleatorio
+        }
+        if(this.position[0] > this.position[2]){
+            let aux = this.position[0];
+            this.position[0] = this.position[2];
+            this.position[2] = aux;
+        }
+        if(this.position[1] < this.position[3]){
+            let aux = this.position[1];
+            this.position[1] = this.position[3];
+            this.position[3] = aux;
+        }
+    }
+
+    initCirc(){
+        for(let i=0;i<this.length-1;i++){
+            this.position[i] = Math.random()*101;   // Posicion aleatorio
+            this.velocity[i] = Math.random()*101;   // Velocidad aleatorio
+        }
+        this.position[this.length-1] = Math.random()*51;
     }
 
     getPosition(){
