3b759c90bc5470246a212a92ec82134190a611d2
Zait06 <zait-hdez@hotmail.com>
2020-06-24 20:31:04 -0500

    PSO

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import { Rectangulo } from './js/Rectangulo'; // Importamos la clase Rectangulo
 import { Circulo } from './js/Circulo';
 import { Particle,PSO } from './js/PSO';
 const { UI } = require('./js/UI');
+var nj = require('@aas395/numjs');
 
 const ui = new UI();
 var rec = new Rectangulo(5,25,20,10);
@@ -13,7 +14,15 @@ var pso_alg = new PSO(10,1,'r')
 
 ui.render(rec.getArea());
 pso_alg.run_pso();
-console.log(pso_alg.population);
+//console.log(pso_alg.population);
+var position = nj.random(4).multiply(100)
+var jeje = []
+for(let i=0;i<4;i++)
+    jeje[i] = Math.random()*101;   // Posicion aleatorio
+var otro = nj.array(jeje);
+console.log(otro);
+console.log(position.get(0));
+console.log(position.tolist());
 //console.log(part00.getPosition());
 //console.log(part01.getPosition());
 
