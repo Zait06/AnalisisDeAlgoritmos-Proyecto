c6f55762e7b82fe7af129aabf54d14ef45d734cf
Zait <zait-hdez@hotmail.com>
2024-02-09 23:06:21 -0600

    Coordinate plane style

--- src-frontend/js/index.js
+++ src-frontend/js/index.js
@@ -3,17 +3,17 @@
 const rectangleForm = `
           Point 1
           <div class="coords-container">
-            <label for="x01">x<sub>1</sub>:</label>
-            <input class="valueShape" type="number" id="x01" step="10" value="0" placeholder="x" min="0" max="800"/>
+          <label for="x01">x<sub>1</sub>:</label>
+            <input class="valueShape" type="number" id="x01" step="50" value="0" placeholder="x" min="0" max="800"/>
             <label for="y01">y<sub>1</sub>:</label>
-            <input class="valueShape" type="number" id="y01" step="10" value="0" placeholder="y" min="0" max="800"/>
+            <input class="valueShape" type="number" id="y01" step="50" value="0" placeholder="y" min="0" max="800"/>
           </div>
           Point 2
           <div class="coords-container">
             <label for="x02">x<sub>2</sub>:</label>
-            <input class="valueShape" type="number" id="x02" step="10" value="0" placeholder="x" min="0" max="800"/>
+            <input class="valueShape" type="number" id="x02" step="50" value="0" placeholder="x" min="0" max="800"/>
             <label for="y02">y<sub>2</sub>:</label>
-            <input class="valueShape" type="number" id="y02" step="10" value="0" placeholder="y" min="0" max="800"/>
+            <input class="valueShape" type="number" id="y02" step="50" value="0" placeholder="y" min="0" max="800"/>
           </div>
 `
 
@@ -21,12 +21,12 @@ const circleForm = `
   Center
   <div class="coords-container">
              <label for="x01">x<sub>1</sub>:</label>
-            <input class="valueShape" type="number" id="x01" step="10" value="0" placeholder="x" min="0" max="800"/>
+            <input class="valueShape" type="number" id="x01" step="50" value="0" placeholder="x" min="0" max="800"/>
             <label for="y01">y<sub>1</sub>:</label>
-            <input class="valueShape" type="number" id="y01" step="10" value="0" placeholder="y" min="0" max="800"/>
+            <input class="valueShape" type="number" id="y01" step="50" value="0" placeholder="y" min="0" max="800"/>
  </div>
   <label for="radius">Radius:</label>
-  <input class="valueShape" type="number" id="radius" step="10" value="0" placeholder="Radius" min="0" max="800"/>
+  <input class="valueShape" type="number" id="radius" step="50" value="0" placeholder="Radius" min="0" max="800"/>
 `
 
 var comboShape = document.getElementById('shape')
@@ -37,7 +37,7 @@ comboShape.addEventListener('change', (evt) => {
   selectedShape = select.value
   if (select.value === 'rectangle')
     document.querySelector('#shapeForm').innerHTML = rectangleForm;
-  else
+  else if (select.value === 'circle')
     document.querySelector('#shapeForm').innerHTML = circleForm;
 })
 
@@ -70,15 +70,38 @@ btnCreateShape.addEventListener('click', (evt) => {
 function drawRectangle(x, y, width, height) {
   var ctx = canvas.getContext('2d');
   ctx.strokeStyle = "#000";
-  ctx.lineWidth = 5;
-  ctx.strokeRect(x, y, width, height);
+  ctx.fillRect(x, y, width, height);
 }
 
 function drawCircle(x, y, radius) {
   const ctx = canvas.getContext("2d");
   ctx.beginPath();
   ctx.strokeStyle = "#000";
-  ctx.lineWidth = 5;
   ctx.arc(x, y, radius, 0, 2 * Math.PI);
-  ctx.stroke();
+  ctx.fill();
 }
+
+function drawAxis() {
+  const axisX = document.getElementById('axis-x')
+  const axisY = document.getElementById('axis-y')
+
+  let axisLabel = '';
+  for (let step = 100; step <= 700; step += 100) {
+    axisLabel += `<div class="axis">${step}</div>`
+  }
+
+  const canvasRect = canvas.getBoundingClientRect()
+
+  axisX.innerHTML = axisLabel
+  axisX.style.width = canvas.width + 100 + 'px'
+  axisX.style.left = canvasRect.left + 'px'
+  axisX.style.top = (canvasRect.top - 20) + 'px'
+
+  axisY.innerHTML = axisLabel
+  axisY.style.height = canvas.height + 'px'
+  axisY.style.right = (canvasRect.left - 20) + 'px'
+  axisY.style.top = canvasRect.top + 'px'
+
+}
+
+drawAxis()
